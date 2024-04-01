f3c8c8169960594c8f53a1a34d8b139071fab713
Sam Gerstner <Sam.Gerstner@samgerstner.pro>
2024-04-01 12:42:37 -0700

    Add Basic Project Structure
    Signed-off-by: Sam Gerstner <Sam.Gerstner@samgerstner.pro>

---
+++ src/main/java/pro/samgerstner/probabilityapi/ProbabilityApiApplication.java
@@ -0,0 +1,16 @@
+package pro.samgerstner.probabilityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbabilityApiApplication
+{
+
+   public static void main(String[] args)
+   {
+
+      SpringApplication.run(ProbabilityApiApplication.class, args);
+   }
+
+}
---
+++ src/test/java/pro/samgerstner/probabilityapi/ProbabilityApiApplicationTests.java
@@ -0,0 +1,16 @@
+package pro.samgerstner.probabilityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbabilityApiApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+
+   }
+
+}
