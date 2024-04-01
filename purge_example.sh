f667887a96dcc2b5030d3720187d35f6e72ed7f5
Sam Gerstner <Sam.Gerstner@samgerstner.pro>
2024-04-01 12:44:25 -0700

    Add Example Purge Script
    Signed-off-by: Sam Gerstner <Sam.Gerstner@samgerstner.pro>

---
+++ purge_example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+API_KEY="yourapikeyhere"
+BASE_URL="http://localhost:8080" #Do NOT add a trailing slash to the URL
+VALUE=1
+UNIT="min"
+
+#Send purge request for marbles
+curl "$BASE_URL/api/purge/marbles" -X "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
+
+#Send purge request for dice
+curl "$BASE_URL/api/purge/dice" -X "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
+
+#Send purge request for coins
+curl "$BASE_URL/api/purge/coins" -X "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
