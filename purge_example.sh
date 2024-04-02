52527ece2e6653c1e1f4a1a6310f10ba2d822531
Sam Gerstner <Sam.Gerstner@samgerstner.pro>
2024-04-02 12:25:58 -0700

    Fix Error in Purge Script
    Signed-off-by: Sam Gerstner <Sam.Gerstner@samgerstner.pro>

--- purge_example.sh
+++ purge_example.sh
@@ -6,10 +6,10 @@ VALUE=1
 UNIT="min"
 
 #Send purge request for marbles
-curl "$BASE_URL/api/purge/marbles" -X "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
+curl "$BASE_URL/api/purge/marbles" -X GET -H "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
 
 #Send purge request for dice
-curl "$BASE_URL/api/purge/dice" -X "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
+curl "$BASE_URL/api/purge/dice" -X GET -H "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
 
 #Send purge request for coins
-curl "$BASE_URL/api/purge/coins" -X "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
+curl "$BASE_URL/api/purge/coins" -X GET -H "x-api-key: $API_KEY" -H 'Content-Type: application/json' -d "{\"value\": $VALUE,\"unit\": \"$UNIT\"}"
